638a481581e5aafb1c0663a26834328694ad0d30
L <dav>
2022-12-13 12:04:35 +0200

    change create_parser()

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from pathlib import Path
 
 def create_app():
     app = Flask(__name__)
-    args = create_parser()
-    app.config["DATABASE"] = args.database
-    app.config["FOLDER"] = args.folder
 
-    # create_db(app.config["DATABASE"], app.config["FOLDER"])
+    app.config["DATABASE"] = Path(__file__).parent / 'db/drivers.db'
+    app.config["FOLDER"] = Path(__file__).parent / "separate folder"
+
+    create_db(app.config["DATABASE"], app.config["FOLDER"])
     Swagger(app)
     create_api(app)
     return app
@@ -27,13 +27,7 @@ def create_api(app):
     return api
 
 
-def create_parser(string=None):
-    parser = argparse.ArgumentParser(description='Get drivers statistic')
-    parser.add_argument('--folder', metavar='', help='Specify where the files are located', nargs='?',
-                        default=Path(__file__).parent / "separate folder")
-    parser.add_argument('--database', metavar='', help='Specify where the database are located',
-                        default=Path(__file__).parent / 'db/drivers.db', nargs='?')
-    return parser.parse_args(string)
+
 
 
 
--- db_drivers.py
+++ db_drivers.py
@@ -1,8 +1,16 @@
+import argparse
 import sqlite3
 from flask import Flask, g, current_app
 from models import *
 from report_of_monaco_2018_racing_dan import report
 
+def create_parser(data, folder,string=None):
+    parser = argparse.ArgumentParser(description='Get drivers statistic')
+    parser.add_argument('--folder', metavar='', help='Specify where the files are located', nargs='?',
+                        default=folder)
+    parser.add_argument('--database', metavar='', help='Specify where the database are located',
+                        default=data, nargs='?')
+    return parser.parse_args(string)
 
 def get_drivers_dict(path):
     drivers = {}
@@ -30,14 +38,18 @@ def get_driver_list(folder):
                       company=driver['company'],
                       result=driver['result'])
         drivers_list.append(driver)
+    return drivers_list
 
 
 def create_db(data, folder):
+    args = create_parser(data,folder)
+    data = args.database
+    folder = args.folder
+
     init_db(data)
     with database:
         database.create_tables([Driver])
-        with database.atomic():
-            Driver.bulk_create(get_driver_list(folder)).execute()
+        Driver.insert_many(get_driver_list(folder)).execute()
 
 
 
